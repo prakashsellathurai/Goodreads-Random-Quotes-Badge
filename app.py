0437fff141b4762e763720e355683e1d2a47ac83
prakash <prakashsellathurai@gmail.com>
2021-09-28 15:38:10 +0530

    🧪 tests added

--- app.py
+++ app.py
@@ -2,42 +2,46 @@ from flask import Flask,render_template,request, abort, jsonify,Markup,make_resp
 import requests
 
 
-app = Flask(__name__)
+flaskApp = Flask(__name__)
 
-@app.errorhandler(404)
+@flaskApp.errorhandler(404)
 def resource_not_found(e):
-    return render_template("404.html")
+    return render_template("404.html"),404
 
-@app.route('/', methods=['GET'])
+@flaskApp.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html'),200
 
-@app.route('/getbadge', methods=['GET'])
+def parse_content(content):
+    content = content.decode()
+    content = content.replace(r"\n", "")
+    content = content[16:].replace('\\','').replace("&ldquo;","").replace("&rdquo;","")
+    content = content[:-2].replace("&mdash;"," -  ")
+    content = content[:-6]
+    content = content.split("</h3>    <br/>")[1]
+    content = content[:-8]
+    content = content.split(" <br/>  <br/>  ")[0]
+    content = content.replace(" -  ","  <br/> <style> #author { text-align: right; }</style>  <div id='author'> -   ")
+    content = content + "</div>"
+    return content
+
+@flaskApp.route('/getbadge', methods=['GET'])
 def getbadge():
     try:
         goodReadsProfileUrl = request.args.get('goodReadsUrl')  
         goodReadsprofileId = goodReadsProfileUrl.split('/')[-1]
-        goodReadsQuotesUrl = "https://www.goodreads.com/quotes/widget/" +goodReadsProfileUrl.split('/')[-1]#+ "?v=2"
+        goodReadsQuotesUrl = "https://www.goodreads.com/quotes/widget/" +goodReadsProfileUrl.split('/')[-1]
         
         
         content = requests.get(goodReadsQuotesUrl).content
-        content = content.decode()
-        content = content.replace(r"\n", "")
-        content = content[16:].replace('\\','').replace("&ldquo;","").replace("&rdquo;","")
-        content = content[:-2].replace("&mdash;"," -  ")
-        content = content[:-6]
-        content = content.split("</h3>    <br/>")[1]
-        content = content[:-8]
-        content = content.split(" <br/>  <br/>  ")[0]
-        content = content.replace(" -  ","  <br/> <style> #author { text-align: right; }</style>  <div id='author'> -   ")
-        content = content + "</div>"
+        content = parse_content(content=content)
         
         template =  render_template('badge.svg',content=content,goodReadsQuotesUrl=goodReadsQuotesUrl,goodReadsProfileUrl=goodReadsProfileUrl)
 
         response = make_response(template)
         response.headers["Content-Type"] = "image/svg+xml"
         response.headers["charset"] = "utf-8"
-        
+        response._status_code = 200
 
         return response
 
---
+++ test_app.py
@@ -0,0 +1,32 @@
+from app import flaskApp
+import pytest
+
+@pytest.fixture
+def client():
+    
+    with flaskApp.test_client() as client:
+        yield client
+        
+def test_index_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    
+def test_get_badge(client):
+    response = client.get('/getbadge?goodReadsUrl=https%3A%2F%2Fwww.goodreads.com%2Fuser%2Fshow%2F105903487-prakash-sellathurai')
+    assert response.status_code == 200
+
+def test_on_get_badge_without_url(client):
+    response = client.get('/getbadge?goodReadsUrl=')
+    assert response.status_code == 404
+
+def test_on_get_badge_without_param(client):   
+    response = client.get('/getbadge')
+    assert response.status_code == 404
+    
+def test_on_get_badge_wit_iilegal_url(client):   
+    response = client.get('/getbadge?goodReadsUrl=foobar')
+    assert response.status_code == 404
+    
+def test_on_get_badge_with_contentYpe(client):
+    response = client.get('/getbadge?goodReadsUrl=https%3A%2F%2Fwww.goodreads.com%2Fuser%2Fshow%2F105903487-prakash-sellathurai')
+    assert response.headers["Content-Type"] == "image/svg+xml"
